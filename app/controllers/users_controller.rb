b9f796d1b5187f869e844275616115f61b45eb0b
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2026-01-15 16:22:19 +0000

    Updated the UsersController to use @user = User.new(allowed_post_params) in the create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(allowed_post_params)
   if @user.save
     redirect_to new_user_path
   else
@@ -13,4 +13,9 @@ class UsersController < ApplicationController
     @user = User.new
     # render :new
   end
+
+  def allowed_post_params
+      params.expect(user: [:username, :email, :password])
+    end
+
 end
