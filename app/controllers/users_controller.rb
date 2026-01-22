d95987564f2dac9e3b6194144d1b9f18f353daa0
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2026-01-22 13:02:26 +0000

    Added user editing. 1 Added edit and update routing. 2 coded edit and update UserController. Created new view edit.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,13 @@ class UsersController < ApplicationController
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(allowed_post_params)
-  if @user.save
-    # flash[:success] = "Great! Your post has been created!"
-    redirect_to new_user_path
-  else
-    # flash.now[:error] = "Rats! Fix your mistakes, please."
-    render :new, status: :unprocessable_entity
-  end
+    if @user.save
+      # flash[:success] = "Great! Your post has been created!"
+      redirect_to new_user_path
+    else
+      # flash.now[:error] = "Rats! Fix your mistakes, please."
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def new
@@ -16,8 +16,42 @@ class UsersController < ApplicationController
     # render :new
   end
 
-  def allowed_post_params
-      params.expect(user: [:username, :email, :password])
-    end
+  def show
+    
+  end
 
+  def edit
+    id = params[:id]
+    unless id.nil?
+      @user = User.find(id)
+    else
+      @user = User.new
+    end
+    # render :edit      
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(allowed_patch_params)
+      # flash[:success] = "Great! Your post has been updated!"
+      redirect_to new_user_path
+    else
+      # flash.now[:error] = "Rats! Fix your mistakes, please."
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    
+  end
+
+  private # Best to make helper methods like this one private
+
+  def allowed_post_params
+    params.expect(user: [:username, :email, :password])
+  end
+  
+  def allowed_patch_params
+    params.expect(user: [:id, :username, :email, :password])
+  end
 end
